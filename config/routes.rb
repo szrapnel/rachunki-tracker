4e9cca270434b11b13c2cd5b777d7146a88d1fba
szrapnel <szrapnel@poczta.onet.pl>
2014-12-31 18:43:10 +0000

    heroku setup

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  
   # root 'welcome#index'
+  
+  root 'invoices#fancy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
