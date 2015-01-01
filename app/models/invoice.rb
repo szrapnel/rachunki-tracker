e1060f6d7b0d9775c4e03cb1b28923196cabab73
szrapnel <szrapnel@poczta.onet.pl>
2015-01-01 12:27:34 +0000

    refac invoices_controller => invoices helper plus filtered method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -9,49 +9,6 @@ class Invoice < ActiveRecord::Base
     create_next(invoice)
   end
 
-  def self.done
-    Invoice.where(done: true)
-  end
-
-  def self.latest
-    list = Operator.all.map { |o| o.invoices.last }
-    list.compact
-
-    list2 = Invoice.where(operator_id: nil)
-    list2 = list2.to_a
-    list2.compact
-    
-    list3 = list + list2
-    list3.compact
-#     Invoice.where(done: true)
-    #     ale tak naprawde to nie last bo daty sa wazne
-  end
-
-  def self.overdue
-#     @due_this_week = current_user.tasks.where(due_date: Date.today..1.week.from_now)
-    
-#     list = Invoice.where(due_date: Date.today..1.week.from_now)
-    list = Invoice.where(['due_date < ?', DateTime.now.to_date])
-  end
-
-    def self.fancy
-#     @due_this_week = current_user.tasks.where(due_date: Date.today..1.week.from_now)
-
-#     list = Invoice.where(due_date: Date.today..2.week.from_now)
-      #       to jest spoko
-
-      list = Invoice.where(['due_date < ?', 2.week.from_now]).where(done: false)
-      list2 = Invoice.where(due_date: nil).where(done: false)
-
-      list3 = list + list2
-
-#     list = Invoice.where(['due_date < ?', DateTime.now.to_date])
-  end
-
-  def self.not_done
-    Invoice.where(done: false)
-  end
-
   def self.execute_as_done(invoice)
     invoice.done = true
     invoice.done_date = DateTime.now.to_date
@@ -61,7 +18,7 @@ class Invoice < ActiveRecord::Base
   def self.create_next(invoice)
     new_invoice = Invoice.new
     new_invoice.operator = invoice.operator
-    new_invoice.due_date = calculate_new_due_date(invoice)
+    new_invoice.due_date = calculate_new_due_date(invoice)f
     new_invoice.title = TitleGenerator::get_next_title(invoice.title)
     new_invoice.done = false
     new_invoice.save
