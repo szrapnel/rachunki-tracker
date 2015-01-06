b524123e5e25f6fc694b2ddebcdac05965feaa6d
szrapnel <szrapnel@poczta.onet.pl>
2015-01-06 13:46:28 +0000

    blocked creating other users accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+#   TODO config
+  validates :email, inclusion: { in: %w(szrapnel@poczta.onet.pl),
+    message: "this is not a valid email" }, on: :create
 end
