74b2f18ef8249ef175620fe9974e73c31282148c
szrapnel <szrapnel@poczta.onet.pl>
2015-01-06 13:46:58 +0000

    blocked creating other users accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   
 #   TODO config
+  #   w zasadzie to zrob po prostu limit userow na 1 i bedzie spoko tez
   validates :email, inclusion: { in: %w(szrapnel@poczta.onet.pl),
     message: "this is not a valid email" }, on: :create
 end
