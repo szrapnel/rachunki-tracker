9f63b7233b8fb2b3b671ca02a7ecc0e9b9472731
szrapnel <szrapnel@poczta.onet.pl>
2015-01-11 18:11:51 +0000

    test fixed

--- test/models/operator_test.rb
+++ test/models/operator_test.rb
@@ -5,19 +5,29 @@ class OperatorTest < ActiveSupport::TestCase
   test "check_if_abandoned true" do
     #     ma dwa stare
     operator = Operator.find 1
-    assert_equal operator.check_if_abandoned?, true
+    assert_equal true, operator.check_if_abandoned?
   end
   
   test "check_if_abandoned false" do
     #     ma jeden prawie nowy
     #     ej ten test padnie po czasie bo jest od today
     operator = Operator.find 2
-    assert_equal operator.check_if_abandoned?, false
+    assert_equal false, operator.check_if_abandoned?
   end
   
   test "check_if_abandoned empty invoices true" do
     operator = Operator.find 4
-    assert_equal operator.check_if_abandoned?, true
+    assert_equal true, operator.check_if_abandoned?
+  end
+  
+  test "get_last_done_date valid date" do
+    operator = Operator.find 1
+    assert_equal '05-01-2012'.to_date, operator.get_last_done_date
+  end
+  
+  test "get_last_done_date valid NEVER DONE" do
+    operator = Operator.find 8
+    assert_equal 'NEVER DONE', operator.get_last_done_date
   end
   
 end
