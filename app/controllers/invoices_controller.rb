3d1b8ea4b73d6217a72df979d3a8711249e60def
szrapnel <szrapnel@poczta.onet.pl>
2015-02-03 12:08:13 +0000

    bugfix

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -91,7 +91,7 @@ class InvoicesController < ApplicationController
     end
 
     def invoice_params
-      params.require(:invoice).permit(:title, :value, :operator_id, :done, :done_date, :due_date, :valid_due_date, :description, :bank_id)
+      params.require(:invoice).permit(:title, :value, :operator_id, :paid, :payment_date, :due_date, :valid_due_date, :description, :bank_id)
     end
   
     def execute_mark_valid_due_date(value)
