4e2f78ca3c7f937233dc55639cad39a2a28de2d3
szrapnel <szrapnel@poczta.onet.pl>
2015-02-08 00:12:47 +0000

    MyMagicController

---
+++ app/controllers/my_magic_controller.rb
@@ -0,0 +1,63 @@
+class MyMagicController < ApplicationController
+  before_action :set_item, only: [:destroy, :edit, :update, :show]
+   
+  def initialize
+    raise 'needs overwrite'
+    raise 'item_params needs it too'
+#     @default_model = Task
+#     @default_redirect_path = '/tasks'
+  end
+  
+  def index
+    @list = @default_model.all
+    render template: 'layouts/magic_view'
+  end
+  
+  def show
+    @item=@item
+    render template: 'layouts/_magic_show'
+  end
+
+  def new
+    @item = @default_model.new
+    render template: 'magic/new'
+  end
+  
+  def edit
+    render template: 'magic/edit'
+  end
+  
+  def create
+    @item = @default_model.new(item_params)
+    if @item.save
+      redirect_to @default_redirect_path, notice: "#{@default_model} created successfully."
+    else
+      render action: 'new'
+    end
+  end
+  
+  def update
+    if @item.update(item_params)
+      redirect_to @default_redirect_path, notice: "#{@default_model} was successfully updated."
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @item.destroy
+    redirect_to @default_redirect_path, notice: "#{@default_model} was successfully destroyed."
+  end
+  
+  private
+  
+    def set_item
+      @item = @default_model.find(params[:id])
+    end
+  
+    def item_params
+      raise 'needs overwrite'
+#       params.require(:task).permit(:title, :description)
+    end
+
+end
